8cc83ca710ace9ad741abb280fb4203aebfc3e54
Luis Champion <luischamp1103@gmail.com>
2024-08-15 22:54:28 +0200

    Operations with digits from 1 to 9

--- functions.js
+++ functions.js
@@ -5,38 +5,75 @@ export function showBtn(btn) {
     const operations = ["+", "-", "/", "x"];
 
     //regex: /[+-/x]/g
-
     if (sessionStorage.getItem("operation")) {
-        //$result.innerHTML
         $result.innerHTML = "";
         sessionStorage.clear();
     }
 
+    // Check if the board has to be clean or not
+    let clearBoard = false;
+
+    // Show result
+    if ($result.innerHTML === "" && !$operation.innerHTML.endsWith("+") && !$operation.innerHTML.endsWith("-") && !$operation.innerHTML.endsWith("x") && !$operation.innerHTML.endsWith("/")) {
+        $result.innerHTML += btn.innerHTML;
+        clearBoard = true;
+    } else { // If a symbol is selected, do operation
+        clearBoard = false;
+    }
 
     // Show operation in progress
     if (operations.includes(btn.innerHTML)) {
+        console.log(`${$result.innerHTML} ${btn.innerHTML}`);
         $operation.innerHTML += `${$result.innerHTML} ${btn.innerHTML} `;
         sessionStorage.setItem("operation", btn.innerHTML);
-    }
-
-    // Show result at the first moment
-    if (!$result.innerHTML || !$operation.innerHTML.endsWith("+")) {
-        $result.innerHTML += btn.innerHTML;
     }
 
     // Show result after operation
     const operation = $operation.innerHTML.split(" ");
 
-    if (operation.length >= 3) {
+    operation.pop();
+    if (operation.length > 3 && operations.includes(operation[operation.length - 1])) {
+        operation.pop();
+    }
+
+    console.log(operation);
+
+    if (operation.length >= 3 && !clearBoard) {
         operation.forEach((number, index) => {
             if (operations.includes(number)) {
-                if (number === "+") {
-                    const num1 = operation[index - 1];
-                    const num2 = operation[index + 1];
-                    console.log(parseInt(num1) + parseInt(num2));
+                let num1, num2, result;
+                if (index >= 3) {
+                    num1 = sessionStorage.getItem("numberProgress");
+                } else {
+                    num1 = operation[index - 1];
                 }
+                num2 = operation[index + 1];
+                switch (number) {
+                    case "+":
+                        result = parseFloat(num1) + parseFloat(num2);
+                        console.log(parseInt(num1) + parseInt(num2));
+                        break;
+                    case "-":
+                        result = parseFloat(num1) - parseFloat(num2);
+                        console.log(parseInt(num1) - parseInt(num2));
+                        break;
+                    case "x":
+                        result = parseFloat(num1) * parseFloat(num2);
+                        console.log(parseInt(num1) * parseInt(num2));
+                        break;
+                    case "/":
+                        result = parseFloat(num1) / parseFloat(num2);
+                        console.log(parseInt(num1) / parseInt(num2));
+                        break;
+                    default:
+                        break;
+                }
+                sessionStorage.setItem("numberProgress", result);
+                $result.innerHTML = result;
             }
         })
+    } else {
+        //console.log("no permitido");
     }
 
 }
